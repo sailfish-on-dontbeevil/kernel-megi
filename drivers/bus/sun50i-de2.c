84223b2ef46e17643b1d59d692ad70f7471a2f02
Ondrej Jirman <megous@megous.com>
2019-09-02 14:51:17 +0200

    sunxi: Use drm_err_probe to handle EPROBE_DEFER errors
    This makes it easier to debug EPROBE_DEFER issues via debugfs.
    
    Signed-off-by: Ondrej Jirman <megous@megous.com>

--- drivers/bus/sun50i-de2.c
+++ drivers/bus/sun50i-de2.c
@@ -16,7 +16,8 @@ static int sun50i_de2_bus_probe(struct platform_device *pdev)
 
 	ret = sunxi_sram_claim(&pdev->dev);
 	if (ret) {
-		dev_err(&pdev->dev, "Error couldn't map SRAM to device\n");
+		dev_err_probe(&pdev->dev, ret,
+			      "Error couldn't map SRAM to device (%d)\n", ret);
 		return ret;
 	}
 
