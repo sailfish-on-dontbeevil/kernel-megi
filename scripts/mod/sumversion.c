84c643de18ce4cf3557875c96388a0d785fd1e1c
Masahiro Yamada <masahiroy@kernel.org>
2020-05-25 00:42:12 +0900

    modpost: avoid false-positive file open error
    One problem of grab_file() is that it cannot distinguish the following
    two cases:
    
     - It cannot read the file (the file does not exist, or read permission
       is not set)
    
     - It can read the file, but the file size is zero
    
    This is because grab_file() calls mmap(), which requires the mapped
    length is greater than 0. Hence, grab_file() fails for both cases.
    
    If an empty header file were included for checksum calculation, the
    following warning would be printed:
    
      WARNING: modpost: could not open ...: Invalid argument
    
    An empty file is a valid source file, so it should not fail.
    
    Use read_text_file() instead. It can read a zero-length file.
    Then, parse_file() will succeed with doing nothing.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/mod/sumversion.c
+++ scripts/mod/sumversion.c
@@ -258,9 +258,11 @@ static int parse_file(const char *fname, struct md4_ctx *md)
 	char *file;
 	unsigned long i, len;
 
-	file = grab_file(fname, &len);
+	file = read_text_file(fname);
 	if (!file)
 		return 0;
+
+	len = strlen(file);
 
 	for (i = 0; i < len; i++) {
 		/* Collapse and ignore \ and CR. */
@@ -287,7 +289,7 @@ static int parse_file(const char *fname, struct md4_ctx *md)
 
 		add_char(file[i], md);
 	}
-	release_file(file, len);
+	free(file);
 	return 1;
 }
 /* Check whether the file is a static library or not */
