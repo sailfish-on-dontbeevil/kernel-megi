94cc1d49af04606cd8ac7c1c57eff1cac406d856
Zi Yan <ziy@nvidia.com>
2020-07-10 14:58:11 +1000

    mm-vmstat-add-events-for-thp-migration-without-split-fix-2
    - Changed THP_MIGRATION_FAILURE as THP_MIGRATION_FAIL per John
    - Dropped all conditional 'if' blocks in migrate_pages() per Andrew and John
    - Updated migration events documentation per John
    - Updated thp_nr_pages variable as nr_subpages for an expected merge conflict
    - Moved all new THP vmstat events into CONFIG_MIGRATION
    - Updated Cc list with Documentation/ and tracing related addresses
    
    Link: http://lkml.kernel.org/r/C5E3C65C-8253-4638-9D3C-71A61858BB8B@nvidia.com
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: Daniel Jordan <daniel.m.jordan@oracle.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1274,6 +1274,9 @@ const char * const vmstat_text[] = {
 #ifdef CONFIG_MIGRATION
 	"pgmigrate_success",
 	"pgmigrate_fail",
+	"thp_migration_success",
+	"thp_migration_fail",
+	"thp_migration_split",
 #endif
 #ifdef CONFIG_COMPACTION
 	"compact_migrate_scanned",
@@ -1320,9 +1323,6 @@ const char * const vmstat_text[] = {
 	"thp_zero_page_alloc_failed",
 	"thp_swpout",
 	"thp_swpout_fallback",
-	"thp_migration_success",
-	"thp_migration_failure",
-	"thp_migration_split",
 #endif
 #ifdef CONFIG_MEMORY_BALLOON
 	"balloon_inflate",
