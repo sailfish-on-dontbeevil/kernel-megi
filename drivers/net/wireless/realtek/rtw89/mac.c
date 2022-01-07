8509aa2d32ffde621d5d54a60ea7c79454cafaea
Ping-Ke Shih <pkshih@realtek.com>
2022-01-07 11:42:24 +0800

    rtw89: add C2H handle of BCN_CNT
    This C2H notify driver the beacon count we send out. We don't handle the
    content for now, so add a dummy handler to avoid messages, like
      rtw89_pci 0000:03:00.0: c2h class 0 func 3 not support
      C2H: 00000000: 01 03 01 3f 0f 00 00 00 80 0a 00 00 00 00 a0
      rtw89_pci 0000:03:00.0: c2h class 0 func 3 not support
      C2H: 00000000: 01 03 01 40 0f 00 00 00 00 03 20 00 00 00 a5
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- drivers/net/wireless/realtek/rtw89/mac.c
+++ drivers/net/wireless/realtek/rtw89/mac.c
@@ -3114,6 +3114,11 @@ rtw89_mac_c2h_log(struct rtw89_dev *rtwdev, struct sk_buff *c2h, u32 len)
 		   RTW89_GET_C2H_LOG_SRT_PRT(c2h->data));
 }
 
+static void
+rtw89_mac_c2h_bcn_cnt(struct rtw89_dev *rtwdev, struct sk_buff *c2h, u32 len)
+{
+}
+
 static
 void (* const rtw89_mac_c2h_ofld_handler[])(struct rtw89_dev *rtwdev,
 					    struct sk_buff *c2h, u32 len) = {
@@ -3130,6 +3135,7 @@ void (* const rtw89_mac_c2h_info_handler[])(struct rtw89_dev *rtwdev,
 	[RTW89_MAC_C2H_FUNC_REC_ACK] = rtw89_mac_c2h_rec_ack,
 	[RTW89_MAC_C2H_FUNC_DONE_ACK] = rtw89_mac_c2h_done_ack,
 	[RTW89_MAC_C2H_FUNC_C2H_LOG] = rtw89_mac_c2h_log,
+	[RTW89_MAC_C2H_FUNC_BCN_CNT] = rtw89_mac_c2h_bcn_cnt,
 };
 
 void rtw89_mac_c2h_handle(struct rtw89_dev *rtwdev, struct sk_buff *skb,
