6846929fb544c94c428da5189c9b18cd429bf3c0
Ondrej Jirman <megi@xff.cz>
2022-09-05 01:02:15 +0200

    usb: dwc3: Track the power state of usb3_generic_phy
    We will need to manage power state of this phy inisde set_mode work,
    without any ability to perform recovery if power on fails, so we'll
    need to track result of power on separately, to be able to balance
    the phy on/off calls.
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -840,7 +840,9 @@ static void dwc3_core_exit(struct dwc3 *dwc)
 	usb_phy_set_suspend(dwc->usb2_phy, 1);
 	usb_phy_set_suspend(dwc->usb3_phy, 1);
 	phy_power_off(dwc->usb2_generic_phy);
-	phy_power_off(dwc->usb3_generic_phy);
+	if (dwc->usb3_phy_powered)
+		phy_power_off(dwc->usb3_generic_phy);
+	dwc->usb3_phy_powered = false;
 
 	usb_phy_shutdown(dwc->usb2_phy);
 	usb_phy_shutdown(dwc->usb3_phy);
@@ -1167,6 +1169,8 @@ static int dwc3_core_init(struct dwc3 *dwc)
 	if (ret < 0)
 		goto err3;
 
+	dwc->usb3_phy_powered = true;
+
 	ret = dwc3_event_buffers_setup(dwc);
 	if (ret) {
 		dev_err(dwc->dev, "failed to setup event buffers\n");
@@ -1289,6 +1293,7 @@ static int dwc3_core_init(struct dwc3 *dwc)
 
 err4:
 	phy_power_off(dwc->usb3_generic_phy);
+	dwc->usb3_phy_powered = false;
 
 err3:
 	phy_power_off(dwc->usb2_generic_phy);
