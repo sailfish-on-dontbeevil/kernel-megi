082844834b663f8ff15d408b5d64be4faa3efb42
Adam Pigg <adam@piggz.co.uk>
2022-12-28 16:41:13 +0000

    Revert "media: sun6i-csi: Add support for multiple endpoints"
    This reverts commit c201ae84a0ddf1d06e5dddf524e0100aae3c7f3c.

--- drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.h
+++ drivers/media/platform/sunxi/sun6i-csi/sun6i_csi.h
@@ -10,7 +10,6 @@
 
 #include <media/v4l2-device.h>
 #include <media/v4l2-fwnode.h>
-#include <media/v4l2-mc.h>
 #include <media/videobuf2-v4l2.h>
 
 #include "sun6i_video.h"
@@ -45,7 +44,10 @@ struct sun6i_csi_config {
 struct sun6i_csi_v4l2 {
 	struct v4l2_device		v4l2_dev;
 	struct media_device		media_dev;
+
 	struct v4l2_async_notifier	notifier;
+	/* video port settings */
+	struct v4l2_fwnode_endpoint	v4l2_ep;
 };
 
 struct sun6i_csi_device {
@@ -67,25 +69,16 @@ struct sun6i_csi_variant {
 	unsigned long	clock_mod_rate;
 };
 
-struct sun6i_csi_async_subdev {
-	struct v4l2_async_subdev	asd; /* must be first */
-
-	/* csi side video port settings for this subdev */
-	struct v4l2_fwnode_endpoint	vep;
-};
-
 /**
  * sun6i_csi_is_format_supported() - check if the format supported by csi
  * @csi_dev:	pointer to the csi device
  * @pixformat:	v4l2 pixel format (V4L2_PIX_FMT_*)
  * @mbus_code:	media bus format code (MEDIA_BUS_FMT_*)
- * @vep:	parsed CSI side bus endpoint configuration
  *
  * Return: true if format is supported, false otherwise.
  */
 bool sun6i_csi_is_format_supported(struct sun6i_csi_device *csi_dev,
-				   u32 pixformat, u32 mbus_code,
-				   struct v4l2_fwnode_endpoint *vep);
+				   u32 pixformat, u32 mbus_code);
 
 /**
  * sun6i_csi_set_power() - power on/off the csi
@@ -100,13 +93,11 @@ int sun6i_csi_set_power(struct sun6i_csi_device *csi_dev, bool enable);
  * sun6i_csi_update_config() - update the csi register settings
  * @csi_dev:	pointer to the csi device
  * @config:	see struct sun6i_csi_config
- * @vep:	parsed CSI side bus endpoint configuration
  *
  * Return: 0 if successful, error code otherwise.
  */
 int sun6i_csi_update_config(struct sun6i_csi_device *csi_dev,
-			    struct sun6i_csi_config *config,
-			    struct v4l2_fwnode_endpoint *vep);
+			    struct sun6i_csi_config *config);
 
 /**
  * sun6i_csi_update_buf_addr() - update the csi frame buffer address
