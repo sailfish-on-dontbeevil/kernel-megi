b5dad47483e911be488367ee3471874cd51eac0b
Ondrej Jirman <megi@xff.cz>
2023-05-21 01:26:59 +0200

    Input: goodix: Try to keep regulator enable/disable balanced
    Failure handling is still not optimal in this driver, but let's
    try to improve it.
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -1502,6 +1502,7 @@ static int goodix_resume(struct device *dev)
 
 		error = regulator_enable(ts->vddio);
 		if (error) {
+			regulator_disable(ts->avdd28);
 			dev_err(dev, "Regulator vddio enable failed.\n");
 			return error;
 		}
