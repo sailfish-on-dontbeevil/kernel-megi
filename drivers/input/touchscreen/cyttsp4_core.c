a6f78b48f49c7cf401ddd56c3a5e984d2c8a8356
Ondrej Jirman <megi@xff.cz>
2024-11-22 23:42:03 +0100

    input: cyttsp4: Fix compile issue
    We can't use {0} initializer with v6.12+
    
    Signed-off-by: Ondrej Jirman <megi@xff.cz>

--- drivers/input/touchscreen/cyttsp4_core.c
+++ drivers/input/touchscreen/cyttsp4_core.c
@@ -851,7 +851,7 @@ static void cyttsp4_get_mt_touches(struct cyttsp4 *cd, int num_cur_tch)
 	struct cyttsp4_sysinfo *si = cd->si;
 	struct cyttsp4_touch tch;
 	int i, j, t = 0;
-	int ids[max(CY_TMA1036_MAX_TCH, CY_TMA4XX_MAX_TCH)] = {0};
+	int ids[max(CY_TMA1036_MAX_TCH, CY_TMA4XX_MAX_TCH)] = {};
 	struct cyttsp4_signal_def* sig;
 
 	for (i = 0; i < num_cur_tch; i++) {
